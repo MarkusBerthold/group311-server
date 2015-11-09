b2124dfc382bc53b1c31a96668332f7efdc47f91
MarkusBerthold <mbert14@student.aau.dk>
2015-11-09 14:38:01 +0100

    Gson

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -1,12 +1,23 @@
 import java.io.BufferedReader;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Serializable;
 import java.net.*;
+
+import com.google.gson.Gson;
 
 public class TCPServer {
 
 	public static void main(String[] args) throws IOException {
-
+		
+		Human Emil = serialiser.fromJson(json, Human.class);
+		
+		
+		Gson g1 = new Gson();
+		
+		String json = "";
+		
 		while (true) {
 			try {
 
@@ -14,9 +25,17 @@ public class TCPServer {
 
 				Socket sock = socket.accept();
 
-				BufferedReader inFromClient = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+				BufferedReader inFromClient = new BufferedReader(new InputStreamReader(sock.getInputStream()));	
+				
+				json = new String(inFromClient.readLine());				
+				
+				g1.toJson(json);
+				
+				//g1.fromJson(json, );
 
-				System.out.println(inFromClient.readLine());
+				//System.out.println(inFromClient.readLine());
+				
+				System.out.println(g1);				
 
 				socket.close();
 
