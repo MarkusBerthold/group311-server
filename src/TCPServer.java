a205166f4c5ab063adb858153703f07222a36b47
MarkusBerthold <mbert14@student.aau.dk>
2015-11-26 13:52:18 +0100

    Now the server can read more than one input, and added IP collector

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -2,41 +2,58 @@ import java.io.BufferedReader;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.*;
-
-
+import java.util.ArrayList;
 
 public class TCPServer {
 
-	public static void main(String[] args) throws IOException {
-		
-		String json = new String ("");
-		
-		Gson serialiser = new Gson();
-		
-		Human Emil = serialiser.fromJson(json, Human.class);
-		
-		while (true) {
-			try {
+	public static ArrayList<Socket> connectionArray = new ArrayList<Socket>();
+	public static ArrayList<String> currentUsers = new ArrayList<String>();
 
-				ServerSocket socket = new ServerSocket(1233);
+	public static void main(String[] args) throws IOException {
+
+		// String json = new String ("");
+
+		// Gson serialiser = new Gson();
+
+		// Human Emil = serialiser.fromJson(json, Human.class);
+
+		ServerSocket socket = new ServerSocket(1330);
+
+		try {
+			while (true) {
 
 				Socket sock = socket.accept();
+				connectionArray.add(sock);
 
-				BufferedReader inFromClient = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-	
-				json = inFromClient.readLine();	//NullPointerException
+				System.out.println("Client connected from: " + sock.getLocalAddress().getHostName());
 
-				serialiser.fromJson(json, Human.class);
-				
-				System.out.println(json);
+				InputStreamReader isr = new InputStreamReader(sock.getInputStream());
 
-				socket.close();
+				BufferedReader inFromClient = new BufferedReader(isr);
 
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				String msg = inFromClient.readLine();
+
+				if (msg != null) {
+					PrintStream ps = new PrintStream(sock.getOutputStream());
+					ps.println("msg received"); // Will be sent to the client
+				}
+				System.out.println(msg);
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			socket.close();
 		}
+
+		// json = inFromClient.readLine(); //NullPointerException
+
+		// serialiser.fromJson(json, Human.class);
+
+		// System.out.println(json);
+
+		// socket.close();
+
 	}
 }
