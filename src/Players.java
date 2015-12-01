1d29345078664e0b5b925a972f8b067051a660dc
bnass <bnass14@student.mail.dk>
2015-12-01 11:16:46 +0100

    Updated Player class

--- src/Players.java
+++ src/Players.java
@@ -1,10 +1,16 @@
 
 import java.net.*;
+import java.util.ArrayList;
 
 public class Players {
 	
 	public Socket sock;
 	public String name;
+	public int numOfTrains;
+	private ArrayList<MissionCard> handOfMissions = new ArrayList<MissionCard>();
+	private ArrayList<TrainCard> handOfTrainCards = new ArrayList<TrainCard>();
+	
+	
 	
 	public Players(Socket sock, String name) {
 		
@@ -12,5 +18,42 @@ public class Players {
 		this.name = name;
 		
 	}
+	
+	Act()
+	{
+		
+	}
+
+	public int getNumOfTrains() {
+		return numOfTrains;
+	}
+
+	public void setNumOfTrains(int numOfTrains) {
+		this.numOfTrains = numOfTrains;
+	}
+
+	public ArrayList<MissionCard> getHandOfMissions() {
+		return handOfMissions;
+	}
+
+	public void setHandOfMissions(ArrayList<MissionCard> handOfMissions) {
+		this.handOfMissions = handOfMissions;
+	}
+
+	public ArrayList<TrainCard> getHandOfTrainCards() {
+		return handOfTrainCards;
+	}
+
+	public void setHandOfTrainCards(ArrayList<TrainCard> handOfTrainCards) {
+		this.handOfTrainCards = handOfTrainCards;
+	}
+	
+	
+	
+	//
+	
+	
+	
+	
 
 }
