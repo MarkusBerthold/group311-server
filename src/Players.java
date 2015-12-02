87a387274225ee712cc3e108de92a1b70f82b373
bnass <bnass14@student.mail.dk>
2015-12-02 23:29:19 +0100

    Player class

--- src/Players.java
+++ src/Players.java
@@ -3,77 +3,119 @@
 import java.net.*;
 import java.util.ArrayList;
 
+import org.newdawn.slick.SlickException;
+
 public class Players {
 	
 	public Socket sock;
 	public int playerNum;
 	public int numOfTrains = 45;
-	private ArrayList<MissionCard> handOfMissions = new ArrayList<MissionCard>();
-	private ArrayList<TrainCard> handOfTrainCards = new ArrayList<TrainCard>();
-	private int r,g,b;
+	public ArrayList<Card> handOfMissions = new ArrayList<Card>();
+	public ArrayList<Card> handOfTrainCards = new ArrayList<Card>();
+	CustomColor plColor;
 	boolean canPlay = false;
 	
-	
-	public Players(Socket sock, int playerNum, int r, int g, int b) {
+	//add socket sock 
+	public Players(int playerNum, CustomColor plColor) {
 		
-		this.sock = sock;
+		//this.sock = sock;
 		this.playerNum = playerNum;
-		this.r = r;
-		this.g = g;
-		this.b = b;
+		this.plColor = plColor;
 	}
 	
 	
 
+	
+	
 	public int getNumOfTrains() {
 		return numOfTrains;
 	}
+
+
+
+
 
 	public void setNumOfTrains(int numOfTrains) {
 		this.numOfTrains = numOfTrains;
 	}
 
-	public ArrayList<MissionCard> getHandOfMissions() {
+
+
+
+
+	public ArrayList<Card> getHandOfMissions() {
 		return handOfMissions;
 	}
 
-	public void setHandOfMissions(ArrayList<MissionCard> handOfMissions) {
+
+
+
+
+	public void setHandOfMissions(ArrayList<Card> handOfMissions) {
 		this.handOfMissions = handOfMissions;
 	}
 
-	public ArrayList<TrainCard> getHandOfTrainCards() {
+
+
+
+
+	public ArrayList<Card> getHandOfTrainCards() {
 		return handOfTrainCards;
 	}
 
-	public void setHandOfTrainCards(ArrayList<TrainCard> handOfTrainCards) {
+
+
+
+
+	public void setHandOfTrainCards(ArrayList<Card> handOfTrainCards) {
 		this.handOfTrainCards = handOfTrainCards;
 	}
-	
+
+
+
+
+
+	public CustomColor getPlColor() {
+		return plColor;
+	}
+
+
+
+
+
+	public void setPlColor(CustomColor plColor) {
+		this.plColor = plColor;
+	}
+
+
+
+
+
 	//Add one or two train cards to player hand
-	public void AddTrainCard(TrainCard TC)
+	public void AddTrainCard(Card TC)
 	{
 		handOfTrainCards.add(TC);
 	}
 	
-	public void AddTrainCard(TrainCard TC1, TrainCard TC2)
+	public void AddTrainCard(Card TC1, Card TC2)
 	{
 		handOfTrainCards.add(TC1);
 		handOfTrainCards.add(TC2);
 	}
 	
 	// add 1-3 mission cards
-	public void AddMissionCard(MissionCard MC)
+	public void AddMissionCard(Card MC)
 	{
 		handOfMissions.add(MC);
 	}
 	
-	public void AddMissionCard(MissionCard MC1, MissionCard MC2)
+	public void AddMissionCard(Card MC1, Card MC2)
 	{
 		handOfMissions.add(MC1);
 		handOfMissions.add(MC2);
 	}
 	
-	public void AddMissionCard(MissionCard MC1, MissionCard MC2, MissionCard MC3)
+	public void AddMissionCard(Card MC1, Card MC2, Card MC3)
 	{
 		handOfMissions.add(MC1);
 		handOfMissions.add(MC2);
@@ -82,13 +124,13 @@ public class Players {
 	
 	
 	
-	public void removeTrainCard(Color color, int num)
+	public void removeTrainCard(String color, int num)
 	{
 		boolean go = false;	//to check that there's enough desired colors in hand
 		int count = 0;
 		for(int i = 0; i<handOfTrainCards.size(); i++ )
 		{
-			if(handOfTrainCards.get(i).getColor() == color)
+			if(handOfTrainCards.get(i).getColor().getColorName() == color)
 			{
 				count++;
 				if(count>=num)
@@ -109,13 +151,43 @@ public class Players {
 			count = 0;
 			for(int i = 0; i<tmpSize; i++ )
 			{
-				if(handOfTrainCards.get(i).getColor() == color && count < num)
+				if(handOfTrainCards.get(i).getColor().getColorName() == color && count < num)
 				{
 					handOfTrainCards.remove(i);
 					count++;
 				}
 			}
+			numOfTrains -= num;
 		}
 		
 	}
+	
 }
+// TEST!!!
+//System.out.println("hej");
+//Board b;
+//try {
+//	 b = new Board(4);
+//
+//Players player1 = new Players(1,b.colors[0]);
+//
+//
+//player1.AddTrainCard(b.trainCards[1], b.trainCards[2]);
+//
+//player1.getHandOfTrainCards();
+//for(int i = 0; i<player1.handOfTrainCards.size(); i++)
+//{
+//System.out.println(player1.handOfTrainCards.get(i).getColor().getColorName());
+//}
+//player1.removeTrainCard("blue", 1);
+//System.out.println(player1.getNumOfTrains());
+//
+//
+//
+//
+//
+//
+//} catch (SlickException e1) {
+//	// TODO Auto-generated catch block
+//	e1.printStackTrace();
+//}
