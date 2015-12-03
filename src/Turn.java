295cdaeaa2ef07174f18cc3f052cb70b46990167
NikolajAndersen <nhan14@student.aau.dk>
2015-12-03 17:30:14 +0100

    printStream in Turn

--- src/Turn.java
+++ src/Turn.java
@@ -8,22 +8,17 @@ import java.util.ArrayList;
 public class Turn {
 
 	int finalTurnCounter;
-	
 
-	int currentPlayer= 0;
-	
+	int currentPlayer = 0;
 
-	
-
+	PrintStream ps;
 	InputStreamReader isr;
 	BufferedReader inFromClient;
-
-	
 
 	// on the initial turn each player is dealt 4 train cards and 2 mission
 	// cards
 
-	void initialTurn(Players[] pArray)throws IOException {
+	void initialTurn(Players[] pArray) throws IOException {
 
 		// to do this we run a for loop to go through a switch case to deal the
 		// cards to each player
@@ -39,73 +34,60 @@ public class Turn {
 				// the cards that goes
 				// to the players hand must override the hand stack in the
 				// Players class
-				
 
 				pArray[0].getHandOfTrainCards();
 				// pArray[0].setHandOfTrainCards(SomeTrainStack);
-				
+
 				pArray[0].getHandOfMissions();
-				//pArray[0].setHandOfMissions(SomeMissionStack);
-				
-				
+				// pArray[0].setHandOfMissions(SomeMissionStack);
 
 				break;
 			case 1:
 				System.out.println("player 2");
 				// update the stack sizes for player 2 to the board and send it
-				
+
 				pArray[1].getHandOfTrainCards();
 				// pArray[1].setHandOfTrainCards(SomeTrainStack);
-				
+
 				pArray[1].getHandOfMissions();
-				//pArray[1].setHandOfMissions(SomeMissionStack);
-				
-				
+				// pArray[1].setHandOfMissions(SomeMissionStack);
 
 				break;
 			case 2:
 				System.out.println("player 3");
 				// update the stack sizes for player 3 to the board and send it
-			
+
 				pArray[2].getHandOfTrainCards();
 				// pArray[2].setHandOfTrainCards(SomeTrainStack);
-				
+
 				pArray[2].getHandOfMissions();
-				//pArray[2].setHandOfMissions(SomeMissionStack);
-				
+				// pArray[2].setHandOfMissions(SomeMissionStack);
+
 				break;
 			case 3:
 				System.out.println("player 4");
 				// update the stack sizes for player 4 to the board and send it
-				
+
 				pArray[3].getHandOfTrainCards();
 				// pArray[3].setHandOfTrainCards(SomeTrainStack);
-				
+
 				pArray[3].getHandOfMissions();
-				//pArray[3].setHandOfMissions(SomeMissionStack);
-				
+				// pArray[3].setHandOfMissions(SomeMissionStack);
+
 				//
 
-				
 				break;
 
 			}
 		}
 	}
 
-	
-
 	// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-	
+
 	// the switch case model to run through each players turn.
-		// takes a players object as an input
+	// takes a players object as an input
 
 	void normalTurn(Players[] pArray, int whoseTurn) throws IOException {
-
-		
-		
-
-		
 
 		switch (whoseTurn) {
 
@@ -114,36 +96,48 @@ public class Turn {
 		// these determine what kind of action the player will perform and apply
 		// the action to the selected player
 		case 1:
-			
 
-			// second action: the player wants to pick up mission cards
-			
-			
+			ps = new PrintStream(pArray[0].sock.getOutputStream(), true);
+			ps.println("canAct");
+
+
 			break;
-
-		// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 		// same as above but for player 2
 
 		case 2:
-			
 
-			// same for player 3
+			ps = new PrintStream(pArray[1].sock.getOutputStream(), true);
+			ps.println("canAct");
+
+			break;
+
+		// same for player 3
 
 		case 3:
-			
 
-			// same for player 4
+			ps = new PrintStream(pArray[2].sock.getOutputStream(), true);
+			ps.println("canAct");
+
+			break;
+
+		// same for player 4
 		case 4:
-			
+
+			ps = new PrintStream(pArray[3].sock.getOutputStream(), true);
+			ps.println("canAct");
+
 			break;
 		}
 
 		// last turn
-		//the finalTurnCounter variable will update so that the final switch case will start 
-		//with the player that plays AFTER a player has less than 2 trains left after his turn
-		//that is why there must be 8 cases. If player 4 is the first to go below 2 trains
-		//then he will have 1 last turn, which will be case 8 here
+		// the finalTurnCounter variable will update so that the final switch
+		// case will start
+		// with the player that plays AFTER a player has less than 2 trains left
+		// after his turn
+		// that is why there must be 8 cases. If player 4 is the first to go
+		// below 2 trains
+		// then he will have 1 last turn, which will be case 8 here
 
 		switch (finalTurnCounter) {
 
@@ -184,16 +178,15 @@ public class Turn {
 		}
 
 	}
-	
-	public int endTurn(){
-		
-		
+
+	public int endTurn() {
+
 		currentPlayer++;
-		
-		if(currentPlayer > 4){
-			currentPlayer= 1;
+
+		if (currentPlayer > 4) {
+			currentPlayer = 1;
 		}
-		
+
 		return currentPlayer;
 	}
 
